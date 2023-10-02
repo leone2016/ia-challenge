7d88778a49c2184cf9e0ffe2a6057a75b6d33fba
lorenaggs <lorenaggs2812@gmail.com>
2023-10-01 20:05:35 -0500

    feat:
    front

--- libs/roaster/src/lib/roaster.component.ts
+++ libs/roaster/src/lib/roaster.component.ts
@@ -1,21 +1,38 @@
 import {UntilDestroy} from "@ngneat/until-destroy";
-import {ChangeDetectionStrategy, Component, OnInit} from "@angular/core";
-import {DatePipe, NgForOf} from "@angular/common";
+import {ChangeDetectionStrategy, Component, OnInit, Injectable} from "@angular/core";
+import {AsyncPipe, DatePipe, NgForOf} from "@angular/common";
+import {selectRoasterData} from "./roaster.selectors";
+import {Store} from "@ngrx/store";
+import {roasterArticleActions} from "./roaster.actions";
+import {RoasterStoreService} from "./roaster.store";
+import {provideComponentStore} from "@ngrx/component-store";
 
 @UntilDestroy()
 @Component({
-  selector: 'cdt-roaster',
-  standalone: true,
-  templateUrl: './roaster.component.html',
-  styleUrls: ['./roaster.component.css'],
-  imports: [
-    DatePipe,
-    NgForOf
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'cdt-roaster',
+    standalone: true,
+    templateUrl: './roaster.component.html',
+    styleUrls: ['./roaster.component.css'],
+    imports: [
+        DatePipe,
+        NgForOf,
+        AsyncPipe
+    ],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [provideComponentStore(RoasterStoreService)]
 })
+
+@Injectable()
 export class RoasterComponent implements OnInit {
-  ngOnInit(): void {
-  }
+    roaster$ = this.homeRoaster.roaster$;
+
+    constructor(
+        private readonly homeRoaster: RoasterStoreService,
+        private readonly store: Store
+    ) {
+    }
+
+    ngOnInit(): void {
+    }
 
 }
